d1a75a660c99d3317b6cc563f3856f0607a309d6
Adam Wong <awong017@ucr.edu>
2020-03-18 20:04:48 -0700

    created sass variables

---
+++ src/Home/home.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import HomeNav from '../HomeNav/homeNav';
+
+const home = () => {
+    return (
+        <div className="home">
+            <HomeNav />
+        </div>
+    )
+}
+
+export default home;
