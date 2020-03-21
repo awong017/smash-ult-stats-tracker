ba24c379507c5286317627d0d2ae87e62130ae3a
Adam Wong <awong017@ucr.edu>
2020-03-21 12:02:17 -0700

    added image with link to landing page

--- src/LandingNav/landingNav.js
+++ src/LandingNav/landingNav.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SmashBall from './images/smash-ball.jpg';
 import './landingNav.css';
 
 const landingNav = () => {
     return (
         <div className="nav">
-            <ul className="landing-nav-ul">
-                <li className="landing-nav-li">
-                    <Link to={"/login"}>
-                        Login
-                    </Link>
-                </li>
-            </ul>
+            <div className="nav-flex">
+                <Link to={"/"} className="smash-ball">
+                    <img className="smash-ball-image" src={SmashBall} />
+                </Link>
+                <ul className="landing-nav-ul">
+                    <li className="landing-nav-li">
+                        <Link to={"/login"}>
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/LoginNav/loginNav.js
+++ src/LoginNav/loginNav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SmashBall from '../LandingNav/images/smash-ball.jpg';
 import './loginNav.css';
 
 const loginNav = () => {
     return (
-        <div className="nav" />
+        <div className="nav">
+            <Link to={"/"} className="smash-ball">
+                <img className="smash-ball-image" src={SmashBall} />
+            </Link>
+        </div>
     )
 }
 
