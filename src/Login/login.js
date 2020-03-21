95f925af04f025d14cdfec37d1af656566dcaca1
Adam Wong <awong017@ucr.edu>
2020-03-20 23:06:53 -0700

    created basic login form layout

--- src/Login/login.js
+++ src/Login/login.js
@@ -7,18 +7,18 @@ const login = () => {
         <div className="login">
             <LoginNav />
             <form className="login-form">
-                <legend>Login</legend>
+                <legend><h2>Login</h2></legend>
                 <fieldset className="login-input">
-                    <label className="name-label">User Name: </label>
+                    <label className="name-label">User Name</label>
                     <input className="name-input" type="text" placeholder="User Name"></input>
 
-                    <label className="password-label">Password: </label>
+                    <label className="password-label">Password</label>
                     <input className="password-input" type="text" placeholder="Password"></input>
 
                     <button className="login-button">Login</button>
                 </fieldset>
                 <fieldset className="login-option">
-                    <p>Forgot password?</p>
+                    <p>Forgot your password?</p>
                     <p>Sign Up</p>
                 </fieldset>
             </form>
