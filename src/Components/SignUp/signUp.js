9de0f2ef524dd26c9312961947e58920d80a8b07
Adam Wong <awong017@ucr.edu>
2020-03-25 16:08:36 -0700

    began implementing handleSignup to Signup component

--- src/Components/SignUp/signUp.js
+++ src/Components/SignUp/signUp.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import './signUp.css'
+import React, { useState, useContext } from 'react';
 import LandingNav from '../LandingNav/landingNav';
+import Context from '../../context';
+import './signUp.css'
 
 const signUp = () => {
+
+    const { handleSignup, emailError, usernameError, passwordError } = useContext(Context)
+
+    const [signupState, setState] = useState("")
+
+    const signUpInfo = (email, username, password1, password2) => {
+        setState({
+            email: email,
+            username: username,
+            password1: password1,
+            password2: password2
+        });
+    }
+
+    const { email, username, password1, password2 } = signupState;
+
     return (
         <div className="sign-up">
             <LandingNav />
-            <form className="sign-up-form">
+            <form className="sign-up-form" onSubmit={(e) => handleSignup(e, email, username, password1, password2)}>
                 <legend><h2>Create Account</h2></legend>
                 <fieldset className="sign-up-input">
                     <label className="email-label">Email</label>
                     <input className="email-input" type="text"></input>
+                    <div className="input-error"></div>
 
                     <label className="name-label">Desired User Name</label>
                     <input className="name-input" type="text"></input>
+                    <div className="input-error"></div>
 
                     <label className="password-label">Password</label>
                     <input className="password-input" type="password"></input>
 
                     <label className="confirm-label">Confirm Password</label>
                     <input className="confirm-input" type="password"></input>
+                    <div className="input-error"></div>
 
-                    <button className="sign-up-button">Sign Up</button>
+                    <input type="submit" className="signup-button" value="Sign Up"></input>
                 </fieldset>
             </form>
         </div>
